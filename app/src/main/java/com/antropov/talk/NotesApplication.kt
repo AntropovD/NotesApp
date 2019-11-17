3b529c769bc2f8c230ee0ee7bf8d773078c31818
Dmitry <antrdima@gmail.com>
2019-11-17 12:48:47 +0500

    Multidex and firebase

--- app/src/main/java/com/antropov/talk/NotesApplication.kt
+++ app/src/main/java/com/antropov/talk/NotesApplication.kt
@@ -1,5 +1,7 @@
 package com.antropov.talk
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.antropov.talk.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
@@ -8,5 +10,10 @@ class NotesApplication : DaggerApplication() {
   override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
     return DaggerApplicationComponent.factory()
         .create(applicationContext)
+  }
+
+  override fun attachBaseContext(base: Context) {
+    super.attachBaseContext(base)
+    MultiDex.install(this)
   }
 }
